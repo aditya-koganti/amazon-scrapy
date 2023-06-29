8d88317c53e1b9914190786bbe6bd723143f3075
aditya-koganti <kogantiaditya02702@gmail.com>
2023-06-28 22:32:58 -0500

    main page: 3, Requsting multiple links from spider

--- spiders/sony_buds.py
+++ spiders/sony_buds.py
@@ -10,19 +10,25 @@ class SonyBudsSpider(scrapy.Spider):
     def parse(self, response):
         item_name = []
         item_price = []
-        
+        item_links = []
         # 2
         item_blocks = response.xpath("//text()[contains(., 'Results')]/following::div[@data-index]//text()[contains(., 'WF-1000XM4')]/ancestor-or-self::div[5]")
         
         for item in item_blocks:
             item_name.append(item.xpath(".//text()[contains(., 'Sony')]").get())
             item_price.append(item.xpath('.//descendant::span[@class="a-offscreen"][1]//text()').get())
+            item_link = item.xpath(".//text()[contains(., 'Sony')]/ancestor-or-self::a[1]/@href").get()
+            item_links.append(item_link)
 
-        yield{
-            # 'item_block': item_block
-            'item_names': item_name,
-            'item_prices': item_price
-        }
+            absolute_url = f"https://www.amazon.com{item_link}"
+            yield scrapy.Request(url=absolute_url)
+       
+        # yield{
+        #     # 'item_block': item_block
+        #     'item_names': item_name,
+        #     'item_prices': item_price,
+        #     'item_links': item_link
+        # }
         
         #================ olds ================#
         # buds_title = response.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[*]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span/text()").getall()
